cf827db2f6046b40a2db89716522034fa46a9a70
Ahmed Gaber <agaber@futureface.sa>
2024-06-09 15:14:18 +0300

    init commit

---
+++ main.js
@@ -0,0 +1,19 @@
+//console.table(["A","B","C","D"])
+///////////////////////
+// chalngr Lesion 17 
+
+
+// let theTitle = "Elzero", 
+//     theDescription = "Elzero Web School", 
+//     theDate = "25/10"
+
+// let divContent = `<h3>${theTitle}</h3>
+//                   <p>${theDescription}</p>
+//                   <span>${theDate}</span>
+//                 `
+
+// for (let index = 0; index < 4 ; index++) {
+//     document.write(divContent);
+// }
+
+///////////////////////////////////////
